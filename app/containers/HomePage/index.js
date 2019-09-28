e67ed7d0ea7a5f7fac00f4e8dba04daf88b3af49
Aylin Malcolm <39786395+avamalcolm@users.noreply.github.com>
2019-09-28 19:40:20 +0100

    Add files via upload

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -40,19 +40,19 @@ export class HomePage extends React.Component {
                   	<h2>UPenn LJS 445 (c. 1440)</h2>
                     <p><img src={im1} width="400" alt="drawing of the Man of Law" style={{ float: 'right' }} /></p>
                     <p>
-                   A late-fifteenth-century astronomical anthology, UPenn LJS 445 contains a diverse array of texts that reveal the importance of astronomical principles for many aspects of medieval life, as well as the fluidity of the boundary between manuscript and early print. Encompassing topics such as medicine, calendrical calculation, and prognostication, this sizeable codex includes material from three print volumes: Johannes Lichtenberger’s <i>Prognosticatio</i>, a book of astronomical predictions first published in 1488, and two editions of Johannes Regiomontanus’s <i>Calendarium</i>, both from the 1470s. In many cases, the scribe reproduces the characteristics of these editions, including title pages and image captions. Yet the layouts and illustrations of LJS 445 also draw on manuscript traditions and benefit from the greater flexibility of handwritten texts in this period, such as the potential for artists to create richly colored images and moveable volvelles.
+                   A late-fifteenth-century astronomical anthology, UPenn LJS 445 contains a diverse array of texts that reveal the importance of astronomy in many facets of medieval life, as well as the fluidity of the boundary between manuscripts and early print. Encompassing topics such as medicine, calendrical calculation, and prognostication, this large codex includes material from three print volumes: Johannes Lichtenberger’s <i>Prognosticatio</i>, a book of astronomical predictions first published in 1488, and two editions of Johannes Regiomontanus’s <i>Calendarium</i>, both from the 1470s. In many cases, the scribe reproduces the characteristics of these editions, including title pages and image captions. Yet the layouts and illustrations of LJS 445 also draw on manuscript traditions and benefit from the greater flexibility of handwritten texts in this period, such as the potential for artists to create richly colored images and moveable volvelles.
                     </p>
                     <p>
                     <img src={im2} width="300" alt="drawing of the Man of Law" style={{ float: 'left' }} />
-					 Currently held at the <a href="https://www.library.upenn.edu/kislak">Kislak Center for Special Collections, Rare Books and Manuscripts</a> at the University of Pennsylvania, Philadelphia, LJS 445 can be traced to Bavaria, and particularly to two of the sons of a Nuremberg patrician, Georg Veit (1573-1606) and Veit Engelhard (1581-1656) Holtzschuher. Its irregular quiring and partial copies of certain texts suggest that this codex was likely rebound at least once in its history. Now composed of 227 leaves with modern finding tabs, it also contains many signs of use, including spaces where images have been extracted, whimsical doodles, and shaky inscriptions by children learning the alphabet.
+					Currently held at the <a href="https://www.library.upenn.edu/kislak">Kislak Center for Special Collections, Rare Books and Manuscripts</a> at the University of Pennsylvania, Philadelphia, LJS 445 can be traced to Bavaria in modern Germany. It is composed of 227 leaves with modern finding tabs and irregular quiring, a fact suggesting that it was rebound at least once in its history. LJS 445 also contains many signs of use, including spaces where images have been extracted, whimsical doodles, and shaky inscriptions by children learning the alphabet. In particular, it appears to have been used and annotated by two sons of a Nuremberg patrician, Georg Veit (1573-1606) and Veit Engelhard (1581-1656) Holtzschuher. 
                     </p>
 					<p>
-					This interactive facsimile of LJS 445 is designed to provide a comprehensive introduction to the many remarkable features of this manuscript. Click on <a href="http://aylinmalcolm.com/ljs445/reader">Browse</a> to scroll through its pages sequentially, or use the <a href="http://aylinmalcolm.com/ljs445/reader/penn/8">Tour this edition</a> option to view a series of “tour stops” highlighting some of its unusual characteristics. Marginal annotations and color-coded categories provide information about the content of each page and the thematic composition of the book, although these categories should be regarded as approximations of concepts that were in fact interconnected, given the medieval belief that celestial bodies exerted influences on terrestrial ones. Finally, the <a href="http://aylinmalcolm.com/ljs445/structure">Structure</a> view uses the VisColl data model to depict the physical makeup of this book, including quires of different sizes as well as missing and inserted leaves.
+					This interactive facsimile of LJS 445 is designed to provide a comprehensive introduction to the many remarkable features of this manuscript. Click on <a href="http://aylinmalcolm.com/ljs445/reader">Browse</a> to scroll through its pages sequentially, or use the <a href="http://aylinmalcolm.com/ljs445/reader/penn/8">Tour</a> option to view a series of “tour stops” highlighting some of its unusual characteristics. Marginal annotations and color-coded categories provide information about the content of each page and the thematic composition of the book, although these categories should be regarded as interconnected, given the medieval belief that celestial bodies exerted influences on terrestrial ones. Finally, the <a href="http://aylinmalcolm.com/ljs445/structure">Structure</a> view uses the <a href="https://github.com/leoba/VisColl">VisColl</a> data model to depict the physical makeup of this book, including quires of different sizes as well as missing and inserted leaves.
 					</p>
 					<h2>About the Editor</h2>
 					<p>
 					<img src={im3} width="300" alt="drawing of the Man of Law" style={{ float: 'right' }} />
-Aylin Malcolm is a Ph.D. candidate in English at the University of Pennsylvania, with research interests in late medieval poetry, the history of ecological science, and manuscript studies. To find out more about Aylin’s work, visit <a href="http://aylinmalcolm.com">aylinmalcolm.com</a>. Please direct all questions and feedback to Aylin at <a href="mailto: malcolma@sas.upenn.edu">malcolma@sas.upenn.edu</a>.
+Aylin Malcolm is a Ph.D. Candidate in English at the University of Pennsylvania, with research interests in late medieval poetry, the history of ecological science, and digital manuscript studies. To find out more about Aylin’s work, visit <a href="http://aylinmalcolm.com">aylinmalcolm.com</a>. Please direct all questions and feedback to Aylin at <a href="mailto: malcolma@sas.upenn.edu">malcolma@sas.upenn.edu</a>.
 					</p>
                     <p className="clear"></p>
 
@@ -60,16 +60,16 @@ Aylin Malcolm is a Ph.D. candidate in English at the University of Pennsylvania,
                 </Col>
                 <Col sm={4}>
                   <ListGroup>
-                    <ListGroupItem> <Link to="/reader/penn/5">
+                    <ListGroupItem> <Link to="/reader/penn/8">
                       <Glyphicon glyph="bookmark" /> Tour this edition</Link>
                     </ListGroupItem>
                     <ListGroupItem >
-                      <Link to="/structure"> <Glyphicon glyph="book" /> Explore the book‘s physical structure</Link>
+                      <Link to="/structure"> <Glyphicon glyph="book" /> Explore the book's physical structure</Link>
                     </ListGroupItem>
                   </ListGroup>
                   <MapView currentPage={0} />
-                  <img src={im4} width="300" alt="engraved portrait of Holzschuher" style={{ float: 'right' }} />
-                  <p style={{ opacity: '.5' }}><i>An engraved portrait of Veit Engelhard Holzschuher as an adult. Holzchuher was eight years old when he owned and annotated this book.</i></p> 
+                  <img src={im4} width="300" alt="engraved portrait of Holtzschuher" style={{ float: 'right' }} />
+                  <p style={{ opacity: '.5' }}><i>An engraved portrait of Veit Engelhard Holtzschuher as an adult. Holzchuher was eight years old when he annotated this book.</i></p> 
 
                 </Col>
               </Row>
